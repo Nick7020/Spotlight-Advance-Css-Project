93bb9254a92ddd48bfdcebfbe3b6ecca9c3c54ca
Nick7020 <nikhilmistari2004@gmail.com>
2026-02-13 20:05:42 +0530

    js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-addEventListener("mousemove",(e)=>{
-    console.log(e.clientX,e.clientY)
-    document.body.style.setProperty("--x",e.clientX+"px")
-    document.body.style.setProperty("--y",e.clientY+"px")
+const para = document.querySelector('p');
+const character="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const text=para.innerHTML
+
+para.addEventListener("mouseenter",(e)=>{
+    setInterval(() => {
+        const str=text.split('').map((char,idx)=>{
+            return character.split("")[Math.floor(Math.random()*53)]
+        }).join("");
+        para.innerHTML=str;   
+    }, 100);
 })
