eab2e76f8fa5096239d0a3c555c84633add5d972
Nick7020 <nikhilmistari2004@gmail.com>
2026-02-15 13:15:26 +0530

    lastest

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
-const para = document.querySelector('p');
-const character="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-const text=para.innerHTML
-
-para.addEventListener("mouseenter",(e)=>{
-    setInterval(() => {
-        const str=text.split('').map((char,idx)=>{
-            return character.split("")[Math.floor(Math.random()*53)]
-        }).join("");
-        para.innerHTML=str;   
-    }, 100);
-})
+const p=document.querySelector('p');
+console.log(p);
